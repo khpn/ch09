0dcfc563b6dad968db76b4439542988875da4e2d
Pavel Khlebaev <pavel_hpn@mail.ru>
2023-01-30 21:12:00 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using static System.Console;
+using System.IO;
+using static System.IO.Directory;
+using static System.IO.Path;
+using static System.Environment;
+
+static void OutputFileSystemInfo()
+{
+    WriteLine($"Path.PathSeparator: {PathSeparator}");
+    WriteLine($"Path.DirectorySeparatorChar: {DirectorySeparatorChar}");
+    WriteLine($"Directory.GetCurrentDirectory(): {GetCurrentDirectory()}");
+    WriteLine($"Environment.CurrentDirectory: {CurrentDirectory}");
+    WriteLine($"Environment.SystemDirectory: {SystemDirectory}");
+    WriteLine($"Path.GetTempPath(): {GetTempPath()}");
+    WriteLine($"GetFolderPath(SpecialFolder):");
+    WriteLine($" System: {GetFolderPath(SpecialFolder.System)}");
+    WriteLine($" ApplicationData:{ GetFolderPath(SpecialFolder.ApplicationData)}");
+    WriteLine($" MyDocuments: {GetFolderPath(SpecialFolder.MyDocuments)}");
+    WriteLine($" Personal: {GetFolderPath(SpecialFolder.Personal)}");
+}
+
+static void Main(string[] args)
+{
+    OutputFileSystemInfo();
+}
